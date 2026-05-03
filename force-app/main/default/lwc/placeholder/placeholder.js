319a93a6e09d30df5906fe8828c16f36bbfc2f1f
Bobby Tripathi <bobbytripathi24@gmail.com>
2026-05-03 23:06:21 +0530

    Created a placeholder Lightning Web Component to display a message and a logo. The component includes an HTML template for the structure and a JavaScript file for the logic. The component is designed to be reusable and can display any message passed to it via the `message` property.

---
+++ force-app/main/default/lwc/placeholder/placeholder.js
@@ -0,0 +1,7 @@
+import { LightningElement, api } from 'lwc';
+import logo from '@salesforce/resourceUrl/CarOnRentalLogo';
+
+export default class Placeholder extends LightningElement {
+    @api message;
+    logoUrl= logo;
+}
